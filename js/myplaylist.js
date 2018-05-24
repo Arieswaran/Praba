3526ee474a8a1cf9c3613ea1b52942a6bf930e1b
Arieswaran <arieswaran@gmail.com>
2018-05-24 22:58:28 +0530

    Update myplaylist.js

--- js/myplaylist.js
+++ js/myplaylist.js
@@ -2,19 +2,20 @@
 
 var myPlaylist = [
 
+   
     {
+      	mp3:'yen.mp3',
+      	title:'yen endral',
+      	artist:'Sorry, I dont know. Too lazy to search',
+      	cover:'music/3.jpg'
+      },  
+     {
         mp3: 'faded.mp3',
      
         title: 'faded',
         artist: 'Alan Walker',
         cover: 'music/1.jpg'
     },
-    {
-      	mp3:'yen.mp3',
-      	title:'yen endral',
-      	artist:'Sorry, I dont know. Too lazy to search',
-      	cover:'music/3.jpg'
-      },   
      {
          mp3: 'Idhazhin.mp3',
          title: 'Idhazhin Oram',
