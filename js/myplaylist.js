6471776cca181cbae7ac53a68b58c5eb7de68010
Arieswaran <arieswaran@gmail.com>
2018-05-24 22:48:52 +0530

    Add files via upload

---
+++ js/myplaylist.js
@@ -0,0 +1,31 @@
+﻿
+
+var myPlaylist = [
+
+    {
+        mp3: 'faded.mp3',
+     
+        title: 'faded',
+        artist: 'Alan Walker',
+        cover: 'music/1.jpg'
+    },
+    {
+      	mp3:'yen.mp3',
+      	title:'yen endral',
+      	artist:'Sorry, I dont know. Too lazy to search',
+      	cover:'music/3.jpg'
+      },   
+     {
+         mp3: 'Idhazhin.mp3',
+         title: 'Idhazhin Oram',
+         artist: 'Anirudh',
+         cover: 'music/2.jpg'
+     },
+      {
+          mp3: 'Oh.mp3',
+          title: 'Oh Oh',
+          artist: 'Anirudh',
+          cover: 'music/3.jpg'
+      }
+      
+];
